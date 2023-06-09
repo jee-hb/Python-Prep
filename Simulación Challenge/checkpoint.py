5ae564e02179b6f2bbe9c29aeed3fd2ae5786221
jee-hb <gabrielaherediabaek@gmail.com>
2023-06-08 21:02:41 -0300

    cambios

--- Simulación Challenge/checkpoint.py
+++ Simulación Challenge/checkpoint.py
@@ -2,15 +2,7 @@
 # código dentro de las funciones ya definidas.
 
 def Factorial(numero):
-    '''
-    Esta función devuelve el factorial del número pasado como parámetro.
-    En caso de que no sea de tipo entero y/o sea menor que 1, debe retornar nulo.
-    Recibe un argumento:
-        numero: Será el número con el que se calcule el factorial
-    Ej:
-        Factorial(4) debe retornar 24
-        Factorial(-2) debe retornar nulo
-    '''
+
     factorial = 1
     if type(numero) != int or numero <1:
         return None
@@ -19,16 +11,7 @@ def Factorial(numero):
     return factorial
 
 def EsPrimo(valor):
-    '''
-    Esta función devuelve el valor booleano True si el número reibido como parámetro es primo, de lo 
-    contrario devuelve False..
-    En caso de que el parámetro no sea de tipo entero debe retornar nulo.
-    Recibe un argumento:
-        valor: Será el número a evaluar
-    Ej:
-        EsPrimo(7) debe retornar True
-        EsPrimo(8) debe retornar False
-    '''
+
     #valor = 1
 
     if type(valor) != int:
@@ -46,23 +29,7 @@ def EsPrimo(valor):
     return es_primo
     
 def ClaseAnimal(especie, color):
-    '''
-    Esta función devuelve un objeto instanciado de la clase Animal, 
-    la cual debe tener los siguientes atributos:
-        Edad    (Un valor de tipo de dato entero, que debe inicializarse en cero)
-        Especie (Un valor de tipo de dato string)
-        Color   (Un valor de tipo de dato string)
-    y debe tener el siguiente método:
-        CumplirAnios  (este método debe sumar uno al atributo Edad y debe devolver ese valor)
-    Recibe dos argumento:
-        especie: Dato que se asignará al atributo Especie del objeto de la clase Animal
-        color: Dato que se asignará al atributo Color del objeto de la clase Animal
-    Ej:
-        a = ClaseAnimal('perro','blanco')
-        a.CumpliAnios() -> debe devolver 1
-        a.CumpliAnios() -> debe devolver 2
-        a.CumpliAnios() -> debe devolver 3
-    '''
+
 
 
     # class Animal:
